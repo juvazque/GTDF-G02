f6b9d941e4b2ddf410de59cc3323e52a3ff263e7
Raúl López Calvo <raullopezcalvo25@gmail.com>
2021-11-16 08:56:27 +0100

    Base de parte de users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/users/list/list.component';
+import { AddComponent } from './components/users/add/add.component';
+import { EditComponent } from './components/users/edit/edit.component';
+import { FooterComponent } from './components/fragments/footer/footer.component';
+import { HeadComponent } from './components/fragments/head/head.component';
+import { NavComponent } from './components/fragments/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    AddComponent,
+    EditComponent,
+    FooterComponent,
+    HeadComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
