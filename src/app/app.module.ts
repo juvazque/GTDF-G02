1faf8ab8cbb5548ebf550ddcb4b4480ec6ab3eb9
santiagova1 <77778517+santiagova1@users.noreply.github.com>
2021-11-16 11:49:45 +0100

    Creation of Tasks' Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddtaskComponent } from './components/tasks/addtask/addtask.component';
+import { EdittaskComponent } from './components/tasks/editTask/edittask.component';
+import { ListtaskComponent } from './components/tasks/listTask/listtask.component';
+import { FooterComponent } from './components/fragments/footer/footer.component';
+import { HeadComponent } from './components/fragments/head/head.component';
+import { NavComponent } from './components/fragments/nav/nav.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddtaskComponent,
+    EdittaskComponent,
+    ListtaskComponent,
+    FooterComponent,
+    HeadComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
